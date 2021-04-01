e97aa2b35cacccc4556d55def29f36eeb865d199
unknown <durotimijohnson@gmail.com>
2021-04-02 00:08:04 +0400

    Added more functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'file:///C:/Users/Durotimi/FlutterProjects/book_app/lib/screens/splash.dart';
+import 'package:book_app/screens/auth/login.dart';
+import 'package:book_app/screens/auth/register.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(BookApp());
